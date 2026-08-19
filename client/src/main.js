c839cf5fc0d0b38e844070744a3046c01e2597dc
msteusnf65 <feufufuejfefjufjef@proton.me>
2026-08-19 14:21:43 -0300

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1718,7 +1718,6 @@ $('modalGo').addEventListener('click', async () => {
   url.searchParams.set('q', $('mQuality').value);
   url.searchParams.set('fps', $('mFps').value);
   url.searchParams.set('som', $('mAudio').checked ? '1' : '0');
-  url.searchParams.set('camera', $('mCamera').checked ? '1' : '0');
   url.searchParams.set(
   'camera',
   $('mCamera').checked ? '1' : '0'
